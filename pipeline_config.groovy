c8664a7d1547cff9ad71929f528f06b1d118ce44
Jeffrey Chiang <45471360+jeff-a-chiang@users.noreply.github.com>
2019-01-15 18:04:04 -0500

    Update pipeline_config.groovy

--- pipeline_config.groovy
+++ pipeline_config.groovy
@@ -1,3 +1,8 @@
+sdp_image_repository = "http://0.0.0.0:5000"
+sdp_image_repository_credential = "sdp-docker-registry"
+application_image_repository = "0.0.0.0:5000"
+application_image_repository_credential = "sdp-docker-registry"
+
 stages{
   continuous_integration{
     static_code_analysis
