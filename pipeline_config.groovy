102261c1f023203b4323a0706a57ed3b61fcdd52
Jeffrey Chiang <45471360+jeff-a-chiang@users.noreply.github.com>
2019-01-16 12:04:05 -0500

    Update pipeline_config.groovy

--- pipeline_config.groovy
+++ pipeline_config.groovy
@@ -3,15 +3,9 @@ sdp_image_repository_credential = "sdp-docker-registry"
 application_image_repository = "0.0.0.0:5000"
 application_image_repository_credential = "sdp-docker-registry"
 
-stages{
-  continuous_integration{
-    static_code_analysis
-    build
-  }
-}
-
 libraries{
   sdp
+  github_enterprise
   sonarqube{
     enforce_quality_gate = true
   }
