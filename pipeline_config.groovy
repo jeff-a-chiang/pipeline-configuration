a2f27077e80f571d0828de5ae2801bf338a139a0
Jeffrey Chiang <45471360+jeff-a-chiang@users.noreply.github.com>
2019-01-24 17:09:49 -0500

    Update pipeline_config.groovy

--- pipeline_config.groovy
+++ pipeline_config.groovy
@@ -1,13 +1,17 @@
-sdp_image_repository = "http://0.0.0.0:5000"
-sdp_image_repository_credential = "sdp-docker-registry"
 application_image_repository = "0.0.0.0:5000"
-application_image_repository_credential = "sdp-docker-registry"
-
-libraries{
-  sdp
-  github_enterprise
-  sonarqube{
-    enforce_quality_gate = true
-  }
-  docker
-}
+    application_image_repository_credential = "sdp-docker-registry"
+    libraries{
+      sdp{
+        images{
+          registry = "http://0.0.0.0:5000" // registry url
+          cred = "sdp-docker-registry"// jenkins cred id to authenticate
+          repo = "sdp"       // repo to find sdp images -> currently hard coded as "sdp"
+          docker_args = "--network=try-it-out_sdp"  // docker runtime args
+        }
+      }
+      github_enterprise
+      sonarqube{
+        enforce_quality_gate = true
+      }
+      docker
+    }
